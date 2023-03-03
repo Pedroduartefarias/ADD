13c947767fbc68898d5252683f157813e4c37b7e
Pedroduartefarias <RM98469@fiap.com.br>
2023-03-03 16:21:29 -0300

    aula 01

---
+++ src/ddd2/casa.java
@@ -0,0 +1,17 @@
+package ddd2;
+
+public class casa { 
+	//Atributos
+	//modificador tipo nomeAtributo;
+    public String sexo;
+    public String idade;
+    public String nome;
+
+
+//Métodos
+ public void
+exibirDadosPessoais() {
+	 
+ }
+ }
+
